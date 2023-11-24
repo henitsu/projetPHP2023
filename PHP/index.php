76773f1c7c899858ad5af42a47dfba0d26a15b72
Mirindra RANDRIANARISON <mirindra.randrianarison-ratsiandavana@etu.iut-tlse3.fr>
2023-11-24 11:47:13 +0100

    Update index.php

--- PHP/index.php
+++ PHP/index.php
@@ -1,18 +1,33 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Gestion d'un cabinet médical</title>
-        <h1>Gestion d'un cabinet médical - Authentification</h1>
-        <form>
-            <label>Identifiant</label>
-            <br>
-            <input type="text" placeholder = "identifiant"><br>
-            <label>Mot de passe</label><br>
-            <input type="password">
-            <a href="#" id="mdp">Mot de passe oublié</a><br><br>
+<head>
+    <meta charset="utf-8">
+    <title>Page</title>
+    <link href="index.css" rel="stylesheet">
+</head>
+    <title>Gestion d'un cabinet médical</title>
 
-            <input id="valider" type="button" value="Se connecter">
-        </form>
+    <div id="page">
 
-    </head>
+        <div id="auth">
+            <h1>Authentification</h1>
+        
+            <form>
+                <label>Identifiant</label>
+                <br>
+                <input type="text" placeholder = "identifiant"><br><br>
+                <label>Mot de passe</label><br>
+                <input type="password"><br>
+                <a href="#" id="mdp">Mot de passe oublié</a><br> <br>
+
+                <input id="valider" type="button" value="Se connecter">
+            </form>
+        </div>
+
+        <div id="partieDroite">
+            <h1>Gestionnaire d'un cabinet médical</h1>
+        </div>
+    </div>
+
+    
 </html>
