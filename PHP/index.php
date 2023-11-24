e54baca6dd576f4d7b40faffad5e8bafb708fb98
Mirindra RANDRIANARISON <mirindra.randrianarison-ratsiandavana@etu.iut-tlse3.fr>
2023-11-24 11:43:19 +0100

    modifs

--- PHP/index.php
+++ PHP/index.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Gestion d'un cabinet médical</title>
-    <h1>Gestion d'un cabinet médical - Accueil</h1>
-    <form>
-        <input type="button" value="Consulter">   
-        <input type="button" value="Ajouter">
-    </form>
-</head>
+    <head>
+        <title>Gestion d'un cabinet médical</title>
+        <h1>Gestion d'un cabinet médical - Authentification</h1>
+        <form>
+            <label>Identifiant</label>
+            <br>
+            <input type="text" placeholder = "identifiant"><br>
+            <label>Mot de passe</label><br>
+            <input type="password">
+            <a href="#" id="mdp">Mot de passe oublié</a><br><br>
+
+            <input id="valider" type="button" value="Se connecter">
+        </form>
+
+    </head>
 </html>
