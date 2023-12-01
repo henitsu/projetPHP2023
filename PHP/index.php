7036c66883a661919382ca026a6ef1c05497746b
Mirindra RANDRIANARISON <mirindra.randrianarison-ratsiandavana@etu.iut-tlse3.fr>
2023-12-01 11:41:24 +0100

    modif css index

--- PHP/index.php
+++ PHP/index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>Gestion d'un cabinet médical</title>
+    <link rel="stylesheet" href="/projetPHP2023/CSS/base.css">
     <link rel="stylesheet" href="/projetPHP2023/CSS/index.css">
 </head>
 <body>
