cfb61925c80da8ee42b08dd4ffc8cb640ff946bd
lilouuuuuuu <lilou.pedro@etu.iut-tlse3.fr>
2023-12-01 12:22:36 +0100

    affichage OK css à faire

--- PHP/affichage.php
+++ PHP/affichage.php
@@ -7,54 +7,52 @@
     <link rel="stylesheet" href="/projetPHP2023/CSS/affichage.css">
 </head>
 <body>
-<h1>Affichage des patients</h1>
-<?php
-    // Vérifie si le formulaire a été soumis
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    <h1>Affichage des patients</h1>
+    <?php
+   
+    // Connexion à la base de données (à personnaliser avec vos propres informations de connexion)
+    $connexion = new mysqli('localhost', 'etu1', 'iutinfo', 'patientele');
 
-        // Connexion à la base de données (à personnaliser avec vos propres informations de connexion)
-        $connexion = new mysqli('localhost', 'etu1', 'iutinfo', 'patientele');
-
-        // Vérifie la connexion
-        if ($connexion->connect_error) {
-            die('Erreur de connexion à la base de données : ' . $connexion->connect_error);
-        }
-
-        // Prépare la requête SQL
-        $requete = "SELECT * FROM USAGER";
-
-        // Exécute la requête
-        $resultat = $connexion->query($requete);
-
-        // Vérifie s'il y a des résultats
-        if ($resultat->num_rows > 0) {
-            echo '<h2>Votre patientèle :</h2>';
-            echo '<table border="1">';
-            echo '<tr><th>ID</th><th>Nom</th><th>Prénom</th><th>Adresse</th><th>Date naissance</th><th>Lieu naissance</th><th>Numéro sécurité sociale</th></tr>';
-
-            // Affiche les résultats
-            while ($row = $resultat->fetch_assoc()) {
-                echo '<tr>';
-				echo '<td>' . $row['ID_Usager'] . '</td>';
-                echo '<td>' . $row['nom'] . '</td>';
-                echo '<td>' . $row['prenom'] . '</td>';
-                echo '<td>' . $row['adresse'] . '</td>';
-                echo '<td>' . $row['dateNaissance'] . '</td>';
-                echo '<td>' . $row['lieuNaissance'] . '</td>';
-                echo '<td>' . $row['numSecu'] . '</td>';
-				echo '<td><a href="modifierPatient.php?nom=' . $row['nom'] . '&prenom=' . $row['prenom'] . '&adresse=' . $row['adresse'] . '&dateNaissance=' . $row['dateNaissance'] . '&lieuNaissance=' . $row['lieuNaissance'] . '&numSecu=' . $row['numSecu'] . '">Modifier</a> | 
-				<a href="supprimer.php?nom=' . $row['nom'] . '&prenom=' . $row['prenom'] . '&adresse=' . $row['adresse'] . '&dateNaissance=' . $row['dateNaissance'] . '&lieuNaissance=' . $row['lieuNaissance'] . '&numSecu=' . $row['numSecu'] . '">Supprimer</a></td>';
-                echo '</tr>';
-            }
-
-            echo '</table>';
-        } else {
-            echo '<p>Aucun contact trouvé avec les mots-clés saisis.</p>';
-        }
-
-        // Ferme la connexion à la base de données
-        $connexion->close();
+    // Vérifie la connexion
+    if ($connexion->connect_error) {
+        die('Erreur de connexion à la base de données : ' . $connexion->connect_error);
     }
+
+    // Prépare la requête SQL
+    $requete = "SELECT * FROM usager";
+
+    // Exécute la requête
+    $resultat = $connexion->query($requete);
+
+    // Vérifie s'il y a des résultats
+    if ($resultat->num_rows > 0) {
+        echo '<h2>Votre patientèle :</h2>';
+        echo '<table border="1">';
+        echo '<tr><th>ID</th><th>Nom</th><th>Prénom</th><th>Adresse</th><th>Date naissance</th><th>Lieu naissance</th><th>Numéro sécurité sociale</th></tr>';
+
+        // Affiche les résultats
+        while ($row = $resultat->fetch_assoc()) {
+            echo '<tr>';
+            echo '<td>' . $row['idusager'] . '</td>';
+            echo '<td>' . $row['Nom'] . '</td>';
+            echo '<td>' . $row['Prenom'] . '</td>';
+            echo '<td>' . $row['Adresse'] . '</td>';
+            echo '<td>' . $row['DateNaissance'] . '</td>';
+            echo '<td>' . $row['LieuNaissance'] . '</td>';
+            echo '<td>' . $row['NumSecu'] . '</td>';
+            echo '<td><a href="modifierPatient.php?nom=' . $row['Nom'] . '&prenom=' . $row['Prenom'] . '&adresse=' . $row['Adresse'] . '&dateNaissance=' . $row['DateNaissance'] . '&lieuNaissance=' . $row['LieuNaissance'] . '&numSecu=' . $row['NumSecu'] . '">Modifier</a> | 
+            <a href="supprimer.php?nom=' . $row['Nom'] . '&prenom=' . $row['Prenom'] . '&adresse=' . $row['Adresse'] . '&dateNaissance=' . $row['DateNaissance'] . '&lieuNaissance=' . $row['LieuNaissance'] . '&numSecu=' . $row['NumSecu'] . '">Supprimer</a></td>';
+            echo '</tr>';
+        }
+
+        echo '</table>';
+    } else {
+        echo '<p>Aucun contact trouvé avec les mots-clés saisis.</p>';
+    }
+
+    // Ferme la connexion à la base de données
+    $connexion->close();
+    
     ?>
 
 </body>
