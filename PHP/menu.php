a785594747ecb85bd02639a053f43e940ec51bba
Mirindra RANDRIANARISON <mirindra.randrianarison-ratsiandavana@etu.iut-tlse3.fr>
2023-12-01 11:45:07 +0100

    Update menu.php

--- PHP/menu.php
+++ PHP/menu.php
@@ -17,7 +17,7 @@
         </ul>
     </header>
     <main>
-        <h1>Bienvenue, <?php $_GET[""] ?></h1>
+        <h1>Bienvenue, <?php ?></h1>
         <div class="grid">
             <div id="usagers" class="box">
                 <h2>Usagers</h2>
