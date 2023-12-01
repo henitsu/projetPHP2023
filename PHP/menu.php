0ebc6e44c5f4db11b80290279629007d2f921c37
Mirindra RANDRIANARISON <mirindra.randrianarison-ratsiandavana@etu.iut-tlse3.fr>
2023-12-01 11:37:14 +0100

    menu global (html, css)

---
+++ PHP/menu.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Menu</title>
+    <link rel="stylesheet" href="/CSS/menu.css">
+    <link rel="stylesheet" href="/CSS/base.css">
+</head>
+<body>
+    <header>
+        <ul>
+            <li>Usagers</li>
+            <li>Médecins</li>
+            <li>Consultations</li>
+            <li>Statistiques</li>
+            <a img="/Donnees/user_account.png" alt="connexion"></a>
+        </ul>
+    </header>
+    <main>
+        <h1>Bienvenue, <?php $_GET["name"] ?></h1>
+        <div class="grid">
+            <div id="usagers" class="box">
+                <h2>Usagers</h2>
+            </div>
+            <div id="medecins" class="box">
+                <h2>Médecins</h2>
+            </div>
+            <div id="consultations" class="box">
+                <h2>Consultations</h2>
+            </div>
+            <div id="statistiques" class="box">
+                <h2>Statistiques</h2>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
