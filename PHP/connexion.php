cd940aed38494b78591c501e000595f94a1c2d42
Mirindra RANDRIANARISON <mirindra.randrianarison-ratsiandavana@etu.iut-tlse3.fr>
2023-12-15 12:09:25 +0100

    ajout connexion.php

--- PHP/connexion.php
+++ PHP/connexion.php
@@ -1,10 +1,14 @@
 <?php
+    // récupération du nom et prénom de la secrétaire connectée
     session_start();
     $login = $_SESSION['identifiant'];
-    $password = $_SESSION['password'];
 
     $pdo = new PDO("mysql:host=localhost;dbname=patientele", "etu1", "iutinfo");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    $sql = "SELECT nom, prenom FROM secretaire WHERE Login = '$login'";
+    $result = $pdo->query($sql);
+    $row = $result->fetch(PDO::FETCH_ASSOC);
 
+    echo $row['prenom'] . " " . $row['nom'];
 ?>
