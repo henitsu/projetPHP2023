981993cc9d5a13b61e9d7804d5369ecce7fb3928
Mirindra RANDRIANARISON <mirindra.randrianarison-ratsiandavana@etu.iut-tlse3.fr>
2023-12-28 12:36:46 +0100

    modif header (en cours)

--- PHP/header.php
+++ PHP/header.php
@@ -6,22 +6,32 @@
 <html lang="fr">
     <link rel="stylesheet" href="/projetPHP2023/CSS/header.css">
 <body>
-    <header>
-        <ul class="menuheader">
-            <li><a href="/projetPHP2023/PHP/menu.php">Menu principal</a></li>
-            <li><a href="/projetPHP2023/PHP/affichagePatient.php">Usagers</a></li>
-            <li><a href="/projetPHP2023/PHP/affichageMedecin.php">Médecins</a></li>
-            <li><a href="/projetPHP2023/PHP/affichageRDV.php">Consultations</a></li>
-            <li><a href="">Statistiques</a></li>
-            <li><a href="#" class="display-picture"><img src="https://i.pravatar.cc/85" alt="profil"></a></li>
-        </ul>
-        <div class="card hidden">
+    <div class="header">
+        <nav>
+            <img src="/projetPHP2023/Donnees/doctor.png" class="logo" alt="doctor">
             <ul>
-                <li><a href="/projetPHP2023/PHP/profil.php">Compte</a></li>
-                <li><a href="/projetPHP2023/PHP/deconnexion.php">Se déconnecter</a></li>
+                <li><a href="/projetPHP2023/PHP/menu.php">Menu principal</a></li>
+                <li><a href="/projetPHP2023/PHP/affichagePatient.php">Usagers</a></li>
+                <li><a href="/projetPHP2023/PHP/affichageMedecin.php">Médecins</a></li>
+                <li><a href="/projetPHP2023/PHP/affichageRDV.php">Consultations</a></li>
+                <li><a href="">Statistiques</a></li>
             </ul>
-        </div>
-    </header>
-    <script src="/projetPHP2023/JS/header.js"></script>
+            <img src="https://i.pravatar.cc/85" class="user-pic" alt="profil">
+            <div class="sub-menu-wrap">
+                <div class="sub-menu">
+                    <div class="user-info">
+                        <img src="https://i.pravatar.cc/85" alt="profil" class="user-pic">
+                        <span class="user-name"><?php echo $_SESSION['prenom'] . " " . $_SESSION['nom']?></span>
+                        <hr>
+                    </div>
+                    <ul>
+                        <li><a href="/projetPHP2023/PHP/profil.php">Compte</a></li>
+                        <li><a href="/projetPHP2023/PHP/deconnexion.php">Se déconnecter</a></li>
+                    </ul>
+                </div>
+            </div>
+            <script src="/projetPHP2023/JS/header.js"></script>
+        </nav>
+    </div>
 </body>
 </html>
