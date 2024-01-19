aceea1171e48932e45cfababf19d8df23072f03c
lilouuuuuuu <lilou.pedro@etu.iut-tlse3.fr>
2024-01-19 12:13:17 +0100

    Suppression /projet-PHP/

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="utf-8">
     <title>Gestion d'un cabinet médical</title>
-    <link rel="shortcut icon" href="/projetPHP2023/Donnees/patientele_icon.ico" />
-    <link rel="stylesheet" href="/projetPHP2023/CSS/base.css">
-    <link rel="stylesheet" href="/projetPHP2023/CSS/index.css">
+    <link rel="shortcut icon" href="/Donnees/patientele_icon.ico" />
+    <link rel="stylesheet" href="/CSS/base.css">
+    <link rel="stylesheet" href="/CSS/index.css">
 </head>
 <body>
 	<div class="container" id="container">
 		<div class="form-container log-in-container">
-			<form action="/projetPHP2023/PHP/menu.php" method="post">
+			<form action="/PHP/menu.php" method="post">
 				<h1>Authentification</h1>
 				<input name="identifiant" type="text" placeholder="Identifiant" />
 				<input type="password" placeholder="Mot de passe" />
@@ -22,7 +22,7 @@
 			<div class="overlay">
 				<div class="overlay-panel overlay-right">
 					<h1>Gestion d'un cabinet médical</h1>
-					<img src="/projetPHP2023/Donnees/doctor.png" alt="docteur">
+					<img src="/Donnees/doctor.png" alt="docteur">
 				</div>
 			</div>
 		</div>
