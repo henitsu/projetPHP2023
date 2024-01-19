92387fdf3a0a6666a179d7d32a87d0b573ec6ef3
Mirindra RANDRIANARISON <mirindra.randrianarison-ratsiandavana@etu.iut-tlse3.fr>
2024-01-19 18:46:55 +0100

    Update modifierRDV.php

--- PHP/modifierRDV.php
+++ PHP/modifierRDV.php
@@ -37,15 +37,45 @@
 				<div class="form">
 					<form action="modifierRDV.php?idusager=<?php echo $idusager;?>&Id_Medecin=<?php echo $Id_Medecin ?>" method="post">
 						<label for="nom_medecin">Nom médecin :</label>
-						<input type="text" id="nom_medecin" name="nom_medecin" value="<?php echo $nom_medecin; ?>" readonly="readonly">
+						<select name="nom_medecin">
+							<?php 
+								$requete = "SELECT nom FROM medecin ORDER BY nom";
+								$statement = $bdd->prepare($requete);
+								$statement->execute();
+								$medecins = $statement->fetchAll(PDO::FETCH_ASSOC);
+								foreach ($medecins as $medecin) {
+									if (strcmp($nom_medecin, $medecin['nom']) == 0) {
+										echo "<option value='" . $medecin['nom'] . "' selected>" . $medecin['nom'] . "</option>";
+									}
+									else {
+										echo "<option value='" . $medecin['nom'] . "'>" . $medecin['nom'] . "</option>";
+									}
+								}
+							?>						
+						</select>
 						<br><br>
 
 						<label for="nom_usager">Nom usager :</label>
-						<input type="text" id="nom_usager" name="nom_usager" value="<?php echo $nom_usager; ?>" readonly="readonly">
+						<select name="nom_usager">
+							<?php 
+								$requete = "SELECT nom FROM usager ORDER BY nom";
+								$statement = $bdd->prepare($requete);
+								$statement->execute();
+								$usagers = $statement->fetchAll(PDO::FETCH_ASSOC);
+								foreach ($usagers as $usager) {
+									if (strcmp($nom_usager, $usager['nom']) == 0) {
+										echo "<option value='" . $usager['nom'] . "' selected>" . $usager['nom'] . "</option>";
+									}
+									else {
+										echo "<option value='" . $usager['nom'] . "'>" . $usager['nom'] . "</option>";
+									}
+								}
+							?>						
+						</select>
 						<br><br>
 						
 						<label for="dateHeure">Date heure :</label>
-						<input type="datetime-local" id="dateHeure" name="dateHeure" value="<?php echo $rdv['DateHeureRDV']; ?>" required><br>
+						<input type="datetime-local" id="dateHeure" name="dateHeure" value="<?php echo $rdv['DateHeureRDV']; ?>" readonly="readonly"><br>
                         <br><br>
 
                         <label for="duree">Durée :</label>
@@ -70,10 +100,11 @@
 				$ids = $etat->fetch(PDO::FETCH_ASSOC);
 				$id = $ids['Id_Medecin'];
 
-                $sql = "UPDATE rdv SET dateHeureRDV = :dateHeureRDV, dureeConsultationMinutes = :dureeConsultationMinutes WHERE idusager = :idusager";
+                $sql = "UPDATE rdv SET dureeConsultationMinutes = :dureeConsultationMinutes WHERE idusager = :idusager";
                 
 				$stmt = $bdd->prepare($sql);
 				$stmt->bindParam(':idusager', $idusager, PDO::PARAM_STR);
+				$stmt->bindParam(':Id_Medecin', $id, PDO::PARAM_STR);
 				$stmt->bindParam(':dureeConsultationMinutes', $duree, PDO::PARAM_STR);
                 $stmt->bindParam(':dateHeureRDV', $dateHeure, PDO::PARAM_STR);
 				$stmt->execute();
